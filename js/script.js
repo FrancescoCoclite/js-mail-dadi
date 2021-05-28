139aa829a8602d958e2e7e08684aa1e125b051dd
Francesco <francescococlite@gmail.com>
2021-05-28 14:12:35 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ console.log(computerVince);
 
 var utenteVince = Math.floor(Math.random() * 6) +1;
 console.log(utenteVince);
+
+
+
+if (computerVince > utenteVince)
+{
+    document.getElementById('win') .innerHTML= 'computer vince'
+    console.log('computer vince');
+}
+else if(computerVince < utenteVince)
+{   
+    document.getElementById('win') .innerHTML= 'utente vince'
+    console.log('utente vince');
+}
+else{
+    document.getElementById('win') .innerHTML= 'nessuno vince'
+    console.log('nessuno vince');
+}
